9dd2a16a84dad21396f931337914273c90b0b3f2
Samardzhiev <nikolay.samardzhiev@sap.com>
2018-03-30 18:08:03 +0300

    First steps to Git authentication

--- src/Register.java
+++ src/Register.java
@@ -1,8 +1,11 @@
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,138 +21,106 @@ import org.apache.commons.validator.routines.EmailValidator;
 @WebServlet("/Register")
 public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Register() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-//	/**
-//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//		
-//		PrintWriter writer=response.getWriter();
-//		writer.println("<!DOCTYPE html>");
-//		writer.println("<html>");
-//		writer.println("<body>");
-//		writer.println("<h2>Register</h1>");
-//		writer.println("<form method =\"POST\" action=\"/NewWeb/Register\">");
-//		writer.println("  Register username:<br>");
-//		writer.println("<input type=\"text\" name=\"username\">");
-//		writer.println("  <br>");
-//		writer.println("  Register password:<br>");
-//		writer.println("  <input type=\"text\" name=\"password\">");
-//		writer.println("  <br>");
-//		writer.println("  Repeat password:<br>");
-//		writer.println("  <input type=\"text\" name=\"passwortRepeat\">");
-//		writer.println("  <br><br>");
-//		writer.println("  <input type=\"submit\" value=\"Register\">");
-//		writer.println("</form> ");
-//		writer.println("</body>");
-//		writer.println("</html>");
-//		writer.close();
-//	}
-
-
-    
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private static final String JDBC_PASS = "matematika6";
+
+	public Register() {
+		super();
+	    try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		String passwordRepeat = request.getParameter("passwortRepeat");
-		
+
+		String status="Registration successful";
 		try {
-			attemptRerister(username,password,passwordRepeat);
+			attemptRerister(username, password, passwordRepeat);
 		} catch (RegistrationException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			status=e.getMessage();
 		}
-		
-		PrintWriter writer=response.getWriter();
-		writer.println("<!DOCTYPE html>");
-		writer.println("<html>");
-		writer.println("<body>");
-		writer.println("Register attempt with parameters : username:" + username 
-				+ " password:" + password
-				+ " repeated password:" + passwordRepeat);
-		writer.println("</body>");
-		writer.println("</html>");
+
+		PrintWriter writer = response.getWriter();
+		writer.println(status);
 		writer.close();
-	
+
 	}
-    private void attemptRerister(String email,String password,String repeatedPassword) throws RegistrationException, SQLException
-    {
-    	
-    	if(!emailExists(email))
-    		throw new RegistrationException("Email does not exist");
-    	if(checkEmailAlreadyTaken(email))
-    		throw new RegistrationException("This email is already in use");
-    	if(!password.equals(repeatedPassword))
-    		throw new RegistrationException("Passwords don't match");
-    	
-    
-		registerInDatabase(email,password);
 
-    }
-   
-    public static void main(String[] args) throws SQLException {
-		Register reg=new Register();
-//		String user="user";
-//		String password="pass";
-//		System.out.println("insert into users('user_name','user_pass') values('"
-//    			+ user+"','"
-//   			+ password +"');");
-//		System.out.println
-//    	("insert into user_roles('user_name','role_name') values('"
-//    			+ user+"','"
-//    			+ "user" +"');");
-		//reg.registerInDatabase(null, null);
+	private void attemptRerister(String email, String password,
+			String repeatedPassword) throws RegistrationException, SQLException {
+
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost/project", "root", JDBC_PASS);
+
+		if (!emailExists(email))
+			throw new RegistrationException("Email does not exist");
+		if (checkEmailAlreadyTaken(connection,email))
+			throw new RegistrationException("This email is already in use");
+		if (!password.equals(repeatedPassword))
+			throw new RegistrationException("Passwords don't match");
+
+		registerInDatabase(connection,email, password);
+
+		connection.close();
 	}
-	
-    private void registerInDatabase(String user,String password) throws SQLException
-    {
-    	Connection connection=DriverManager.getConnection("jdbc:mysql://localhost/project",
-    			"root",
-    			"TooMuchSwag69");
-    	
-    	Statement statement=connection.createStatement();
-    	//('user_name','user_pass')
-    	statement.executeUpdate
-    	("insert into users values('"
-    			+ user+"','"
-    			+ password +"');");
-    	//('user_name','role_name')
-    	statement.executeUpdate
-    	("insert into user_roles values('"
-    			+ user+"','"
-    			+ "user" +"');");
-    	
-    	connection.close();
-    	
-    }
-    
-    
-    
-    private boolean emailExists(String email)
-    {
-    	boolean valid = EmailValidator.getInstance().isValid(email);
-    	return valid;
-    }
-    
-    private boolean checkEmailAlreadyTaken(String email)
-    {
-    	return false;
-    }
-    
-    
 
+	//for testing
+	public static void main(String[] args) throws SQLException {
+		Register reg = new Register();
+		String email="email";
+		
+		System.out.println
+		("select * from users where user_name='" + email+"';");
+		// String user="user";
+		// String password="pass";
+		// System.out.println("insert into users('user_name','user_pass') values('"
+		// + user+"','"
+		// + password +"');");
+		// System.out.println
+		// ("insert into user_roles('user_name','role_name') values('"
+		// + user+"','"
+		// + "user" +"');");
+		// reg.registerInDatabase(null, null);
+	}
 
+	private void registerInDatabase(Connection connection,String user, String password) throws SQLException{
+		Statement statement = connection.createStatement();
+		
+		statement.executeUpdate("insert into users values('" + user + "','"
+				+ password + "');");
+
+		statement.executeUpdate("insert into user_roles values('" + user
+				+ "','" + "user" + "');");
+	}
+
+	private boolean emailExists(String email) {
+		boolean valid = EmailValidator.getInstance().isValid(email);
+		return valid;
+	}
+
+	private boolean checkEmailAlreadyTaken(Connection connection,String email) throws SQLException {
+		Statement statement = connection.createStatement();
+		
+		ResultSet res = statement
+				.executeQuery("select * from users where user_name='" + email+"';");
+
+		return res.next();
+	}
 
 }
--- src/UserDatabase.java
+++ src/UserDatabase.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -20,87 +19,87 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/admin/UserDatabase")
 public class UserDatabase extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UserDatabase() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+
+	private static final String JDBC_PASS = "matematika6";
+	
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UserDatabase() {
+		super();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		String method=request.getParameter("method");
-		
-		switch(method)
-		{
-		case "clear" :
-			
-			break;
-		default :
-			break;
-		}
-		
-    	Connection connection;
-    	PrintWriter writer=response.getWriter();
-    	
-		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/project",
-						"root",
-						"TooMuchSwag69");
-	
-	    	Statement statement=connection.createStatement();
-	
-	    	ResultSet res1=statement.executeQuery("select * from users;");
-	    	
-	    	statement=connection.createStatement();
-	    	
-	    	ResultSet res2=statement.executeQuery("select * from user_roles;");
-	    	
-	    	while(res1.next())
-	    	{
-	    		res2.next();
-	    		writer.println(res1.getString("user_name") + " " + res1.getString("user_pass") + " | Confirm user: "+
-	    				res2.getString("user_name") + " Role: " + res2.getString("role_name"));
-	    	}
-	    	
-			switch(method)
-			{
-			case "clear" :
-				writer.println("Running command : "+method);
-				clearUsers(connection);
+
+		String method = request.getParameter("method");
+		if (method != null) {
+			switch (method) {
+			case "clear":
+
 				break;
-			default :
+			default:
 				break;
 			}
-	    
-	    	writer.close();
-	    	
-	    	connection.close();
-    	
+		}
+		Connection connection;
+		PrintWriter writer = response.getWriter();
+
+		try {
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost/project", "root", JDBC_PASS);
+
+			Statement statement = connection.createStatement();
+
+			ResultSet res1 = statement.executeQuery("select * from users;");
+
+			statement = connection.createStatement();
+
+			ResultSet res2 = statement
+					.executeQuery("select * from user_roles;");
+
+			while (res1.next()) {
+				res2.next();
+				writer.println(res1.getString("user_name") + " "
+						+ res1.getString("user_pass") + " | Confirm user: "
+						+ res2.getString("user_name") + " Role: "
+						+ res2.getString("role_name"));
+			}
+
+			switch (method) {
+			case "clear":
+				writer.println("Running command : " + method);
+				clearUsers(connection);
+				break;
+			default:
+				break;
+			}
+
+			writer.close();
+
+			connection.close();
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	private void clearUsers(Connection connection) throws SQLException
-	{
-		Statement stmt=connection.createStatement();
-		//stmt.update
+	private void clearUsers(Connection connection) throws SQLException {
+		Statement stmt = connection.createStatement();
+		// stmt.update
 	}
-	
-//	/**
-//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		doGet(request, response);
-//	}
+
 
 }
